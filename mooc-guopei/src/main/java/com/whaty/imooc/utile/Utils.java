2dea60aca758ff24d541cef04a71c1fa112cb105
sdfkg <lsl@163.com>
2017-09-04 18:19:04 +0800

    测试git

--- mooc-guopei/src/main/java/com/whaty/imooc/utile/Utils.java
+++ mooc-guopei/src/main/java/com/whaty/imooc/utile/Utils.java
@@ -7,7 +7,7 @@ public class Utils {
 	static final String LOG_TAG = "PullToRefresh";
 
 	public static void warnDeprecation(String depreacted, String replacement) {
-		Log.w(LOG_TAG, "You're using the deprecated " + depreacted + " attr, please switch over to " + replacement);
+		Log.w(LOG_TAG, "You're using the dsfdeprecated " + depreacted + " attr, please switch over to " + replacement);
 	}
 
 }
